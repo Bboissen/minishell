4bab4788ef8e06c18c13a2177c43d38c2dd444a7
Bruno Boissenin <bboissen@z1r4p3.42lyon.fr>
2024-03-13 16:56:06 +0100

    basic readline protected with no leaks - ignore leaks file

--- src/clear.c
+++ src/clear.c
@@ -6,57 +6,42 @@
 /*   By: bboissen <bboissen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 16:26:06 by bboissen          #+#    #+#             */
-/*   Updated: 2024/03/11 16:28:48 by bboissen         ###   ########.fr       */
+/*   Updated: 2024/03/13 16:15:36 by bboissen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//cc -o clear src/clear.c -lreadline -ltermcap
+
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <unistd.h>
-#include <term.h>
-#include <stdlib.h>
-
-//cc -o clear src/clear.c -lreadline -ltermcap
+#include <errno.h>
 
 int main(void)
 {
-    char *rl;
-    char *pwd;
-    char term_buffer[2048];
-    char *clear_seq;
-    char *term_type = getenv("TERM");
+	char *rl;
 
-    if (tgetent(term_buffer, term_type) < 1) {
-        fprintf(stderr, "Could not get terminal entry\n");
-        return 1;
-    }
-
-    while (1)
-    {
-        rl = readline("Prompt > ");
-        if (rl == NULL) {
-            continue;
-        }
-        add_history(rl);
-        if (rl[0] == 'c') {
-            clear_seq = tgetstr("cl", NULL);
-            if (clear_seq != NULL) {
-                tputs(clear_seq, 1, putchar);
-            } else {
-                fprintf(stderr, "Could not get clear sequence\n");
-            }
-        }
-        if (rl[0] == 'p') {
-            pwd = getcwd(NULL, 0);
-            printf("%s\n", pwd);
-            free(pwd);
-        }
-        if (rl[0] == 'b') {
-            break;
-        }
-        rl_on_new_line();
-        free(rl);
-    }
-    return (0);
+	while (1)
+	{
+		rl = readline("Prompt > ");
+		free(rl);
+		if (rl == NULL && errno == ENOMEM) 
+		{
+			fprintf(stderr, "Insufficient memory for readline\n");
+			rl_clear_history();
+			return (errno);
+		}
+		if (rl == NULL && errno == 0 || rl[0] == 'e') 
+		{
+			printf("exit\n");
+ 			break;
+		}
+		add_history(rl);
+		if (rl[0] == 'c')
+			write(STDOUT_FILENO, "\033[H\033[J", 7);
+	}
+	rl_clear_history();
+	return (0);
 }
