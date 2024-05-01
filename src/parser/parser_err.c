fbea91386e5818079c72c38f14ed8af4e3bfdb37
Bruno Boissenin <bboissen@z1r4p1.42lyon.fr>
2024-05-01 17:32:46 +0200

    fix: parser err updated

--- src/parser/parser_err.c
+++ src/parser/parser_err.c
@@ -6,7 +6,7 @@
 /*   By: bboissen <bboissen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:03:28 by bbsn              #+#    #+#             */
-/*   Updated: 2024/05/01 17:27:36 by bboissen         ###   ########.fr       */
+/*   Updated: 2024/05/01 17:30:23 by bboissen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	parser_err(t_mini *mini, char *str, int err)
 	t_sig	*sig;
 
 	sig = get_sig();
-	sig->status = err;
+	sig->status = 1; //for file but not for command
 	if (err != MALLOC && err != EXE)
 		ft_printfd(STDERR_FILENO, "%s: %s: %s\n", mini->name, str, strerror(err));
 	if (err == EXE)
